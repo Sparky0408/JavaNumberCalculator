c7abd23a73354ca4bae8fa466b27169f3bca9a36
Ruturaj Shinde <ruturajrs20@gmail.com>
2023-07-25 09:12:06 +0530

    Added logic to calculate the average of three numbers

---
+++ LargestNumberSum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LargestNumberSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int largest = num1;
+
+        if (num2 > largest) {
+            largest = num2;
+        }
+
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+        int sum = largest;
+
+        System.out.println("The sum of the largest number is (" + largest + ") is: " + sum);
+    }
+}
